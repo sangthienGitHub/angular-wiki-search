c45ac0b3f969e2c7de72100cf3d8c3b210e430d6
sangTran <sangthien2010@gmail.com>
2020-05-11 17:47:07 -0500

    communicate wiki search result from app parent to page list chilf component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,13 @@ import { WikipediaService } from './wikipedia.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  pages = [];
   //shortcut constructor implementation
   constructor(private wikipedia: WikipediaService) {}
 
   onTerm(term: string) {
-    this.wikipedia.search(term).subscribe((response) => {
-      console.log(response);
+    this.wikipedia.search(term).subscribe((response: any) => {
+      this.pages = response.query.search;
     });
   }
 }
