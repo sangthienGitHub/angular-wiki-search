6ddda92b5cff23ad76bae2af3e6bb28ab23465fa
sangTran <sangthien2010@gmail.com>
2020-05-13 11:16:19 -0500

    add rxjs example with interface and observable

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,5 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { pluck } from 'rxjs/operators';
+
+interface Car {
+  year: number;
+  color: string;
+  running: boolean;
+  make: {
+    name: string;
+    dateCreated: number;
+  };
+}
+
+const observable = new Observable<Car>((observable) => {
+  observable.next({
+    year: 2000,
+    color: 'red',
+    running: true,
+    make: {
+      name: 'Chevy',
+      dateCreated: 1950,
+    },
+  });
+}).pipe(pluck('make', 'dateCreated'));
+
+observable.subscribe((value) => {
+  console.log(value);
+});
 
 @Injectable({
   providedIn: 'root',
